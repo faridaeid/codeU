c3958f834bd889ab567ef60507d57c2a6e762070
Farida Eid <farida.eid@aucegypt.edu>
2017-06-01 00:00:26 +0200

    New changes

--- QuestionTwoUpdated.cpp
+++ QuestionTwoUpdated.cpp
@@ -13,13 +13,9 @@ using namespace std;
 struct node
 {
     int value;
-    node* next =NULL;
+    node* next =nullptr;
     
-    node(int v)
-    {
-        value = v;
-        next = NULL;
-    }
+    node(int v) : value(v), next(nullptr){}
     
     void setNext(node *nxt)
     {
@@ -29,18 +25,18 @@ struct node
 
 node* findKToLastElement(node* head, int k)
 {
-    if(head == NULL) return NULL;
+    if(head == nullptr) return nullptr;
     
     int length = 1;
     node* temp = head;
     
-    while(temp->next != NULL)
+    while(temp->next != nullptr)
     {
         length++;
         temp = temp->next;
     }
     
-    if(k >= length || k < 0) return NULL;
+    if(k >= length || k < 0) return nullptr;
     
     temp = head;
     
@@ -60,7 +56,7 @@ int main()
     
     node* nodes[10];
     
-    for(int i=1; i <=10; i++)
+    for(int i=0; i <10; i++)
     {
         nodes[i] = new node(i);
         if(i)
@@ -69,7 +65,7 @@ int main()
     
     node* result = findKToLastElement(nodes[0], k);
     
-    for(int i=1; i <=10; i++)
+    for(int i=0; i <10; i++)
     {
         delete nodes[i];
     }
