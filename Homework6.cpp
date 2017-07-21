93cccc1cabcdc7888dc1a2d43f460ae93952fdb4
Farida Eid <farida.eid@aucegypt.edu>
2017-07-21 19:30:58 +0200

    First Draft of code

--- Homework6.cpp
+++ Homework6.cpp
@@ -12,43 +12,89 @@
 
 using namespace std;
 
-class Solution
+
+
+class CarParking
 {
 private:
-    unordered_map<int, int> inputIndexHashTable;
+    unordered_map<int, int> inputPermutationMap, outputPermutationMap;
+    vector<bool> inAChain;
+    int N;
+    vector<unordered_map<int, int>> edges;
 public:
-    Solution(){};
+    CarParking(){};
     void reArrange(vector<int>, vector<int>);
 };
 
-void Solution::reArrange(vector<int> inputPermutation, vector<int> outputPermutation)
+void CarParking::reArrange(vector<int> inputPermutation, vector<int> outputPermutation)
 {
-    for(int i=0; i< inputPermutation.size(); i++)
+    N = inputPermutation.size();
+    inAChain.resize(N, false);
+    
+    for(int i=0; i<N; i++)
     {
-        inputIndexHashTable[inputPermutation[i]] = i;
+        inputPermutationMap[inputPermutation[i]] = i;
+        outputPermutationMap[outputPermutation[i]] = i;
     }
     
-    for(int i=0; i< inputPermutation.size(); i++)
+    for(int num : inputPermutation)
     {
+        if(inputPermutationMap[num] != outputPermutationMap[num])
+        {
+            int current = num;
+            unordered_map <int, int> edge;
+            
+            while(!inAChain[current])
+            {
+                inAChain[current] = true;
+                int nextNumber = inputPermutation[outputPermutationMap[current]];
+                
+                edge[nextNumber] = current;
+                current = nextNumber;
+            }
+            
+            if(edge.size() >= 1) edges.push_back(edge);
+        }
+    }
+    
+    for(auto edge : edges)
+    {
+        if(edge.find(0) == edge.end())
+        {
+            int number = edge.begin()->first;
+            
+            cout<<"Moving "<<inputPermutationMap[number]<<" to "<<inputPermutationMap[0]<<endl;
+
+            swap(inputPermutationMap[0], inputPermutationMap[number]);
+            swap(inputPermutation[inputPermutationMap[0]], inputPermutation[inputPermutationMap[number]]);
+            
+
+            edge[0] = edge[number];
+            edge[number] = 0;
+            
+        }
         
-        int emptyIndex = inputIndexHashTable[0];
-        int swappingNumber = outputPermutation[emptyIndex];
+        int from = 0;
+        do
+        {
+            cout<<"Moving "<<inputPermutationMap[edge[from]]<<" to "<<inputPermutationMap[from]<<endl;
+            
+            //swap(inputPermutationMap[edge[from]], inputPermutationMap[from]);
+           // swap(inputPermutation[inputPermutationMap[edge[from]]], inputPermutation[inputPermutationMap[from]]);
+            
+            from = edge[from];
         
-        if(swappingNumber == 0) continue;
+        }while(edge[from]);
         
-        int whereInInput = inputIndexHashTable[swappingNumber];
-        
-        cout<<"Moving "<<whereInInput<<" to "<<emptyIndex<<endl;
-        
-        inputIndexHashTable[0] = whereInInput;
-        inputIndexHashTable[swappingNumber] = emptyIndex;
+        swap(inputPermutationMap[edge[from]], inputPermutationMap[from]);
+        swap(inputPermutation[inputPermutationMap[edge[from]]], inputPermutation[inputPermutationMap[from]]);
     }
 }
 
 int main()
 {
-    Solution s;
-    vector<int> o = {2, 1,0, 4,3};
-    vector<int> i = {4,3,2,1,0};
+    CarParking s;
+    vector<int> o = {3,1, 0, 4, 2};
+    vector<int> i = {1, 3, 0, 2, 4};
     s.reArrange(i, o);
 }
